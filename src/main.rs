721248cb164d555cc00575fb6284c72906c8b42f
Abhinandan <abhinandanverma551@gmail.com>
2024-10-20 10:37:05 +0530

    advanced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use chrono::{Local, Utc};
+
 fn main() {
     println!("Hello, world!");
     //let ans = is_even(4);
@@ -5,13 +7,27 @@ fn main() {
 
     // println!("fibonaccini value at position 10: {}", fib2(4));
 
-    let str = String::from("Hello world! I am Abhinandan");
-    let lenght = get_str_length(str);
-    println!("Length of the str is {}", lenght);
-}
+    // let str = String::from("Hello world! I am Abhinandan");
+    // let lenght = get_str_length(str);
+    // println!("Length of the str is {}", lenght);
+
+    // let index = find_first_a(String::from("Harkirat"));
+
+    // match index {
+    //     Some(value) =>  println!("First 'a' is at index {}", value),
+    //     None => println!("No 'a' found"),
+    //     }
+    // }
+
+    let now = Utc::now();
+    print!("current time is {}", now);
+
+    let formatted  = now.format("fmt");
 
 fn is_even(num:i32) -> bool {
     return num % 2 == 0
+    }
+
 }
 
 fn fib(num: i32) -> i32 {
@@ -46,4 +62,15 @@ fn fib2(num: i32) -> i32 {
 
 fn get_str_length(s: String) -> usize {
  return s.chars().count();
+}
+
+fn find_first_a(s: String) -> Option<i32> {
+    let mut i = 0;
+    for(index, char) in s.chars().enumerate() {
+        if char == 'a' {
+            return  Some(index as i32)
+        }
+    }
+
+    return  None;
 }
